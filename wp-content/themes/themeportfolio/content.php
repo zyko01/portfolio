efcf3af700a24bee5808cd3370c3a8468da8a822
Mark Biglang-awa <mark.zyko01@gmail.com>
2017-11-09 18:01:05 +0800

    getting the content from the content.php to the index.php

---
+++ wp-content/themes/themeportfolio/content.php
@@ -0,0 +1,3 @@
+<div class="row">
+    <h1>hello im now in the content</h1>
+</div>  
--- wp-content/themes/themeportfolio/index.php
+++ wp-content/themes/themeportfolio/index.php
@@ -1,7 +1,18 @@
 <?php get_header(); ?>
 
-<div class="container">
-    <h1>Hello world</h1>
+<div class="container-fluid">
+    <?php
+        if(have_posts() ) :
+            while(have_posts()) : the_post();
+                get_template_part('content', get_post_format() );
+            
+            endwhile;
+                
+            else:
+                echo '<p>No content found!</p>' ;
+        
+        endif;
+    ?>
 </div>
 
 <?php get_footer(); ?> 
