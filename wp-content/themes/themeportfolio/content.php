44803c505b629f923d1120f501defb9792b0400d
Mark Biglang-awa <mark.zyko01@gmail.com>
2017-11-10 11:12:05 +0800

    navigation menu done except for a little bit styling

--- wp-content/themes/themeportfolio/content.php
+++ wp-content/themes/themeportfolio/content.php
@@ -1,3 +1,9 @@
-<div class="row">
+<div class="row" id="contact" >
+<a href="#contact"></a>
     <h1>hello im now in the content</h1>
-</div>  
+</div> 
+
+<div class="row" id="about">
+<a href="#about"></a>
+    hello you are this is about row
+</div> 
--- wp-content/themes/themeportfolio/functions.php
+++ wp-content/themes/themeportfolio/functions.php
@@ -12,3 +12,16 @@ function themeportfolio_resources(){
     wp_enqueue_script('bootstrapjs', get_template_directory_uri() . '/js/bootstrap.min.js', array(), '3.3.7', 'all' );
 }
     add_action('wp_enqueue_scripts', 'themeportfolio_resources');
+
+function themeportfolio_support(){
+    // add menus to the admin panel
+    get_theme_support('menus');
+
+    // register navigation menus
+    register_nav_menus(array(
+        'primary' => __('Primary Menu'),
+        'footer' => __('Footer Menu')
+    ));
+}
+    add_action('init', 'themeportfolio_support');
+    
