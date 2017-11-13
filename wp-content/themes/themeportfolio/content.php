0967e40c848892111ff489d404c4a7e96f0d2760
Mark Biglang-awa <mark.zyko01@gmail.com>
2017-11-13 22:27:57 +0800

    setting the title of each content.

--- wp-content/themes/themeportfolio/content.php
+++ wp-content/themes/themeportfolio/content.php
@@ -1,13 +1,18 @@
 <div class="sections">
-    <section class="row" id="about" href="#about"> 
-        <h1>hello this is about</h1>
+    <section class="row" id="about" href="#about">
+        <div class="container">
+            <h1>About</h1>
+            <div class="box"></div>
+        </div> 
     </section> 
 
     <section class="row" id="portfolio" href="portfolio">
-        <h1>this is portfolio</h1>
+        <h1>Portfolio</h1>
+        <div class="box"></div>
     </section>
 
     <section class="row" id="contacts" href="contacts">
-        <h1>hello this is contact</h1>
+        <h1>Contacts</h1>
+        <div class="box"></div>
     </section>
 </div> 
