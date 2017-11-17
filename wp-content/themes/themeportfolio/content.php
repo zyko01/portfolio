8d521f124d1959ebb72d7e04d9e6623157ac7606
Mark Biglang-awa <mark.zyko01@gmail.com>
2017-11-18 01:29:29 +0800

    done with the portfolio section

--- wp-content/themes/themeportfolio/content.php
+++ wp-content/themes/themeportfolio/content.php
@@ -51,112 +51,188 @@
                         <img class="port-img" src="<?php echo get_bloginfo('template_url') ?> /images/campuscarpool.png" alt="campus carpool">
                             <p class="title">Campus Carpool</p>
                             <div class="overlay"></div>
-                            <button type="button" class="button" data-toggle="modal" data-target="#myModal">Learn More</button>
-                            
+                            <button type="button" class="button" data-toggle="modal" data-target="#myModal">Learn More</button>    
                     </div>
+                   
                     <div class="portfolio2 col-xs-12 col-md-6">
                         <img class="port-img" src="<?php echo get_bloginfo('template_url') ?> /images/ebarangay.png" alt="barangay inventory system">
                         <p class="title">Barangay Inventory System</p>
                         <div class="overlay"></div>
                         <button type="button" class="button" data-toggle="modal" data-target="#myModal1">Learn More</button>               
+                        
                         <!-- Modal -->
-    <div class="modal fade" id="myModal1" role="dialog">
-                            <div class="modal-dialog">
-                            
-                            <!-- Modal content-->
-                            <div class="modal-content">
-                                <div class="modal-header">
-                                    <button type="button" class="close" data-dismiss="modal">&times;</button>
-                                    <h4 class="modal-title">Barangay Inventory System</h4>
-                                </div>
-                                <div class="modal-body">
-                                    <p>Some text in the modal.</p>
-                                </div>
-                                <div class="modal-footer">
-                                    <button type="button" class="btn btn-info" data-dismiss="modal">Close</button>
-                                </div>
-                            </div>
-
-                    </div>
-                </div>
-                     <!-- Modal -->
-                     <div class="modal fade" id="myModal" role="dialog">
-                            <div class="modal-dialog">
-                            
-                            <!-- Modal content-->
-                            <div class="modal-content">
-                                <div class="modal-body">
+                        <div class="modal fade" id="myModal1" role="dialog">
+                        <div class="modal-dialog">
+                     
+                        <!-- Modal content-->
+                        <div class="modal-content">
+                            <div class="modal-body">
                                 <div id="myCarousel" class="carousel slide" data-ride="carousel">
-                                        <!-- Indicators -->
-                                        <ol class="carousel-indicators">
+                                    <!-- Indicators -->
+                                    <ol class="carousel-indicators">
                                         <li data-target="#myCarousel" data-slide-to="0" class="active"></li>
                                         <li data-target="#myCarousel" data-slide-to="1"></li>
                                         <li data-target="#myCarousel" data-slide-to="2"></li>
                                         <li data-target="#myCarousel" data-slide-to="3"></li>
+                                    </ol>
+
+                                    <!-- Wrapper for slides -->
+                                    <div class="carousel-inner" role="listbox">
+
+                                        <div class="item active">
+                                            <img src="<?php echo get_bloginfo('template_url') ?> /images/borrow-request.png" alt="borrow-request">
+                                            <div class="carousel-caption">
+                                                <h3></h3>
+                                                <p></p>
+                                            </div>
+                                        </div>
+
+                                        <div class="item">
+                                            <img src="<?php echo get_bloginfo('template_url') ?> /images/item-listing.png" alt="Chania">
+                                            <div class="carousel-caption">
+                                                <h3></h3>
+                                                <p></p>
+                                            </div>
+                                        </div>
+                                 
+                                        <div class="item">
+                                            <img src="<?php echo get_bloginfo('template_url') ?> /images/new-item.png" alt="Flower">
+                                            <div class="carousel-caption">
+                                                <h3></h3>
+                                                <p></p>
+                                                </div>
+                                        </div>
+
+                                        <div class="item">
+                                            <img src="<?php echo get_bloginfo('template_url') ?> /images/user-listing.png" alt="Flower">
+                                            <div class="carousel-caption">
+                                                <h3></h3>
+                                                <p></p>
+                                            </div>
+                                        </div>
+                             
+                                    </div> <!-- end of Carousel inner -->
+
+                                    <!-- Left and right controls -->
+                                    <a class="left carousel-control" href="#myCarousel" role="button" data-slide="prev">
+                                    <span class="glyphicon glyphicon-chevron-left" aria-hidden="true"></span>
+                                    <span class="sr-only">Previous</span>
+                                    </a>
+                                    <a class="right carousel-control" href="#myCarousel" role="button" data-slide="next">
+                                    <span class="glyphicon glyphicon-chevron-right" aria-hidden="true"></span>
+                                    <span class="sr-only">Next</span>
+                                    </a>
+                                </div> <!-- end id mycarousel -->
+                            </div> <!-- end modal body -->
+                            
+                            <div class="port-info">
+                                <div class="port-title">
+                                    <h2>Barangay Inventory System</h2>
+                                </div>
+                                <p>The Barangay Inventory System or BIS was developed for any barangay's in Cebu City to use to automate the maintenance jobs related such as inventory tracking, borrowing, maintenance, 
+                                user management, anywhere in a certain barangay. Each barangay will be able to keep track of almost every item that goes in, 
+                                or out of their inventory. The system was built using the codeigniter and bootstrap.
+                                </p>     
+                            </div>
+                         
+                            <div class="modal-footer">
+                             <button type="button" class="btn btn-default" data-dismiss="modal">Close</button>
+                            </div>
+                        </div>  <!-- end modal content -->   
+                        </div>  <!-- end modal dialog -->
+                        </div>  <!-- end modal fade -->
+                
+                    </div> <!-- end of portfolio2 -->
+                     
+                    <!-- Modal campuscarpool -->
+                    <div class="modal fade" id="myModal" role="dialog">
+                        <div class="modal-dialog">
+                            
+                            <!-- Modal content-->
+                            <div class="modal-content">
+                                <div class="modal-body">
+                                    <div id="myCarousel2" class="carousel slide" data-ride="carousel">
+                                        <!-- Indicators -->
+                                        <ol class="carousel-indicators">
+                                            <li data-target="#myCarousel2" data-slide-to="0" class="active"></li>
+                                            <li data-target="#myCarousel2" data-slide-to="1"></li>
+                                            <li data-target="#myCarousel2" data-slide-to="2"></li>
+                                            <li data-target="#myCarousel2" data-slide-to="3"></li>
                                         </ol>
 
                                         <!-- Wrapper for slides -->
                                         <div class="carousel-inner" role="listbox">
 
-                                        <div class="item active">
-                                            <img src="<?php echo get_bloginfo('template_url') ?> /images/html_logo.png" alt="Chania">
-                                            <div class="carousel-caption">
-                                            <h3>Chania</h3>
-                                            <p>The atmosphere in Chania has a touch of Florence and Venice.</p>
+                                            <div class="item active">
+                                                <img src="<?php echo get_bloginfo('template_url') ?> /images/post-ride.png" alt="Chania">
+                                                <div class="carousel-caption">
+                                                    <h3></h3>
+                                                    <p></p>
+                                                </div>
                                             </div>
-                                        </div>
 
-                                        <div class="item">
-                                            <img src="<?php echo get_bloginfo('template_url') ?> /images/css_logo.png" alt="Chania">
-                                            <div class="carousel-caption">
-                                            <h3>Chania</h3>
-                                            <p>The atmosphere in Chania has a touch of Florence and Venice.</p>
+                                            <div class="item">
+                                                <img src="<?php echo get_bloginfo('template_url') ?> /images/browse-route.png" alt="Chania">
+                                                <div class="carousel-caption">
+                                                    <h3></h3>
+                                                    <p></p>
+                                                </div>
                                             </div>
-                                        </div>
                                         
-                                        <div class="item">
-                                            <img src="<?php echo get_bloginfo('template_url') ?> /images/vue_logo.png" alt="Flower">
-                                            <div class="carousel-caption">
-                                            <h3>Flowers</h3>
-                                            <p>Beautiful flowers in Kolymbari, Crete.</p>
+                                            <div class="item">
+                                                <img src="<?php echo get_bloginfo('template_url') ?> /images/request-ride.png" alt="Flower">
+                                                <div class="carousel-caption">
+                                                    <h3></h3>
+                                                    <p></p>
+                                                </div>
                                             </div>
-                                        </div>
 
-                                        <div class="item">
-                                            <img src="<?php echo get_bloginfo('template_url') ?> /images/wordpress_logo.png" alt="Flower">
-                                            <div class="carousel-caption">
-                                            <h3>Flowers</h3>
-                                            <p>Beautiful flowers in Kolymbari, Crete.</p>
+                                            <div class="item">
+                                                <img src="<?php echo get_bloginfo('template_url') ?> /images/private-message.png" alt="Flower">
+                                                <div class="carousel-caption">
+                                                    <h3></h3>
+                                                    <p></p>
+                                                </div>
                                             </div>
-                                        </div>
                                     
-                                        </div>
+                                        </div> <!-- end of carousel inner -->
 
                                         <!-- Left and right controls -->
-                                        <a class="left carousel-control" href="#myCarousel" role="button" data-slide="prev">
+                                        <a class="left carousel-control" href="#myCarousel2" role="button" data-slide="prev">
                                         <span class="glyphicon glyphicon-chevron-left" aria-hidden="true"></span>
                                         <span class="sr-only">Previous</span>
                                         </a>
-                                        <a class="right carousel-control" href="#myCarousel" role="button" data-slide="next">
+                                        <a class="right carousel-control" href="#myCarousel2" role="button" data-slide="next">
                                         <span class="glyphicon glyphicon-chevron-right" aria-hidden="true"></span>
                                         <span class="sr-only">Next</span>
                                         </a>
-                                    </div>
-                                </div>
+                                    </div> <!-- end id mycarousel -->
+                                </div> <!-- end modal body -->
+                                
                                 <div class="port-info">
                                     <div class="port-title">
                                         <h2>Campus Carpool</h2>
                                     </div>
-                                    <p>Campus Carpool is local ridesharing app for the students and employees of University of San Carlos.</p>    
+                                    <p>Campus Carpool is a localized ridesharing application for students and employees
+                                    of Unversity of San Carlos to use as an alternative to public transportation.
+                                    </p> 
+                                    <p>
+                                    Campus Carpool was developed to take advantage of the trend and further promote ridesharing.
+                                     Using the latest in web technologies, Campus Carpool was built with reliable frameworks such as laravel, vue and etc,
+                                     in order to allow students and employees of the campus to seamlessly post or find rides that match their commuting routes.
+                                    </p>
+                                    <a href="http://campuscarpool.dyndns.org">Campus Carpool link</a>     
                                 </div>
                                 <div class="modal-footer">
                                     <button type="button" class="btn btn-default" data-dismiss="modal">Close</button>
                                 </div>
-                            </div>   
-                    </div> 
-   
-            </div>
-        </div>
+                            </div>  <!-- end of modal content --> 
+                        </div> <!-- end of modal dialog -->
+                        </div><!-- end of modal fade -->
+
+                </div> <!-- end of col-xs-12 col-md-12 -->
+            </div><!-- end of content-wrapper -->
+        </div> <!-- end of container -->
     </section>
 
     <section class="row" id="contact" href="contact">
